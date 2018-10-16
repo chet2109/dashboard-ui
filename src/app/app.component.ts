a010d30e758dfc9b4f1ba8918fd7d72fcc44b9c3
chetan <chetan.hsit@gmail.com>
2018-10-16 21:55:53 +0530

    adding files

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css','../assets/css/normalize.css'
+  ,'../assets/css/themify-icons.css','../assets/scss/style.css'
+  ]
+})
+export class AppComponent implements OnInit {
+  
+  constructor() {}
+
+  ngOnInit() {
+
+  }
+}
