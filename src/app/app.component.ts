75eeecdcfcc0d27bfb3ac6accf77e576670422d5
chetan <chetan.hsit@gmail.com>
2018-10-17 21:23:30 +0530

    adding test app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Chart } from 'chart.js';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css','../assets/css/normalize.css'
-  ,'../assets/css/themify-icons.css','../assets/scss/style.css'
-  ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  
-  constructor() {}
-
-  ngOnInit() {
-
-  }
+export class AppComponent {
+  title = 'app';
 }
